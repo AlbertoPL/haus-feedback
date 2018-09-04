5ecbcc5e860d51318929b6d91355fb13b077b097
AlbertoPL <apareja@albertopl.com>
2018-09-03 19:58:24 -0700

    Change environment variable for DB connection string.

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -7,9 +7,8 @@ const User = require("./user.js").User;
 const Feedback = require("./feedback.js").Feedback;
 
 const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${
-    process.env.DB_HOST
-  }:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  process.env.DATABASE_URL ||
+    "postgres://postgres:admin@localhost:5432/feedback-dev"
 );
 
 fs.readdirSync(__dirname)
